7865297db9112fd943aeef5dbd35d603ca5ddb46
BOT22345 <nmongia48@gmail.com>
2024-09-22 17:48:48 +0530

    data from url showing perfectly on screen

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function checkWeather(){
     console.log(data)
 
     document.querySelector(".city").innerHTML=data.name
-    document.querySelector(".temp").innerHTML=data.main.temp +"°C"
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"°C"
     document.querySelector(".humidity").innerHTML=data.main.humidity +" %"
     document.querySelector(".wind").innerHTML=data.wind.speed +" km/h"
 
